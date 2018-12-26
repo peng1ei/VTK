6128fab771dd1525f635cd582bb3bc095c390eed
penglei <2643650181@qq.com>
2018-12-26 18:04:24 +0800

    初始化配置

---
+++ src/examples/rendering/Cylinder.cpp
@@ -0,0 +1,10 @@
+//
+// Created by penglei on 18-12-26.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "This is examples rendering cylinder." << std::endl;
+    return 0;
+}
---
+++ src/temp/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by penglei on 18-12-26.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "This is temp_main." << std::endl;
+    return 0;
+}
